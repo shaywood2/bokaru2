3b9a1e442385d0002a748280ba3a9f96687f4765
Shawn Haywood <shaywood2@gmail.com>
2016-11-02 21:51:20 -0400

    More merge conflicts

--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    displayName = models.CharField(max_length=200, null=False, unique=True)
+    email = models.EmailField(max_length=200, null=False, unique=True)
+    name = models.CharField(max_length=200)
+    birthDate = models.DateField()
+    STATUSES = (
+        ('created', 'Created'),
+        ('active', 'Active'),
+        ('suspended', 'Suspended'),
+        ('deleted', 'Deleted')
+    )
+    status = models.CharField(max_length=20, choices=STATUSES)
+    # Automatic timestamps
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.displayName + ' (' + self.email + ')'
