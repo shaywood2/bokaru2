8e0d10f981cb72e70330e57bc5bffd4c38680996
Shawn Haywood <shaywood2@gmail.com>
2016-11-14 19:26:57 -0500

    Creating base template and styling event and login pages.

--- account/views.py
+++ account/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from registration.backends.hmac.views import RegistrationView as BaseRegistrationView
+
+from .forms import RegistrationForm
+from .models import Account
+
+
+class RegistrationView(BaseRegistrationView):
+    form_class = RegistrationForm
+
+    def register(self, form):
+        new_user = BaseRegistrationView.register(self, form)
+        acc = Account()
+        acc.fullName = form.cleaned_data['fullName']
+        acc.user = new_user
+        acc.status = 'created'
+        acc.save()
 
 
 @login_required
 def summary(request):
+    try:
+        acc = Account.objects.get(user=request.user)
+    except:
+        acc = None
+
     context = {
         'test': "Summary Page",
         'user': request.user,
+        'account': acc
     }
 
     return render(request, 'account/summary.html', context)
--- bokaru/urls.py
+++ bokaru/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from account.views import RegistrationView
+from account.forms import RegistrationForm
 
 urlpatterns = [
     url(r'^', include('web.urls')),
@@ -7,5 +9,11 @@ urlpatterns = [
     url(r'^chat/', include('chat.urls')),
     url(r'^admin/', admin.site.urls),
     # Registration and authentication views
+    url(r'^account/register/$',
+        RegistrationView.as_view(
+            form_class=RegistrationForm
+        ),
+        name='registration_register',
+        ),
     url(r'^account/', include('registration.backends.hmac.urls')),
 ]
