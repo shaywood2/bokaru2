fa4b595479a1255c418e2f210e031d3e28cd2956
Shawn Haywood <shaywood2@gmail.com>
2016-12-05 20:21:19 -0500

    Profile and event pages.

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from datetime import date
 
 from registration.backends.hmac.views import RegistrationView as BaseRegistrationView
 
@@ -49,17 +50,30 @@ def subscription(request):
 def view(request):
     current_user = request.user
     account = Account.objects.get(user=current_user)
+    age = calculate_age(account.birthDate)
+    account.age = age
+
     context = {
         'user': current_user,
         'account': account,
+
     }
 
     return render(request, 'account/view.html', context)
 
 
+def calculate_age(born):
+    today = date.today()
+    years_difference = today.year - born.year
+    is_before_birthday = (today.month, today.day) < (born.month, born.day)
+    elapsed_years = years_difference - int(is_before_birthday)
+    return elapsed_years
+
+
 def view_user(request, username):
     user = get_object_or_404(User, username=username)
     account = Account.objects.get(user=user)
+
     context = {
         'user': user,
         'account': account,
@@ -80,6 +94,30 @@ def edit(request):
         form = AccountForm(instance=account)
 
     return render(request, 'account/edit.html', {'form': form})
+
+
+def password(request):
+    context = {}
+
+    return render(request, 'account/changepassword.html', context)
+
+
+def notifications(request):
+    context = {}
+
+    return render(request, 'account/notifications.html', context)
+
+
+def closeaccount(request):
+    context = {}
+
+    return render(request, 'account/close.html', context)
+
+
+def history(request):
+    context = {}
+
+    return render(request, 'account/history.html', context)
 
 
 def settings(request):
