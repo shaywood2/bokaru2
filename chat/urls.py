fd4a99dee5f98dcc5bf41bcf54692fd6ec4aa5d3
Dmitry Pyryeskin <dmitry@pyryeskin.com>
2017-06-19 08:50:09 -0400

    - Minor bug fixed

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'chat'
 urlpatterns = [
     # ex: /chat/session
-    url(r'^session/$', views.sessionCreate, name='session'),
+    url(r'^session/$', views.session_create, name='session'),
     # ex: /chat/token/12345
-    url(r'^token/(?P<session_id>.+)/$', views.tokenCreate, name='token'),
+    url(r'^token/(?P<session_id>.+)/$', views.token_create, name='token'),
 ]
