ab5fa3f7c941e96a9ce2317691520dd33a70301c
Dmitry Pyryeskin <dpyryeskin@eyereturn.com>
2017-06-21 17:20:51 -0400

    - Added a view to list dates for user - Added functions to generate test data

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^session/$', views.session_create, name='session'),
     # ex: /chat/token/12345
     url(r'^token/(?P<session_id>.+)/$', views.token_create, name='token'),
+    # ex: /chat/dates/event/12345/user/1
+    url(r'^dates/event/(?P<event_id>.+)/user/(?P<user_id>.+)/$', views.get_user_dates, name='get_user_dates'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
 from opentok import OpenTok, MediaModes, OutputModes
+from django.contrib.auth.models import User
+
+from chat import utils
 
 api_key = "45689752"
 api_secret = "8b928a5fcc3d59f30bd1e8577171cef2676edecf"
+
 
 # Initialize API
 # opentok = OpenTok(api_key, api_secret)
@@ -25,3 +29,18 @@ def token_create(request, session_id):
     }
     return JsonResponse(data)
 
+
+def get_user_dates(request, event_id, user_id):
+    date_matrix = utils.get_date_matrix(event_id)
+    user_id_int = int(user_id)
+    if user_id_int in date_matrix:
+        result = {}
+        for date_num in date_matrix[user_id_int]:
+            # Get user
+            # TODO: get account instead of user model
+            user = User.objects.get(pk=int(date_matrix[user_id_int][date_num]))
+            result[date_num] = {'username': user.username, 'id': user.id}
+
+        return JsonResponse(result)
+    else:
+        return JsonResponse({})
