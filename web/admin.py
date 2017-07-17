eb4f5ad06d018a897bd656cfb40e37b7eb2c7c8c
Dmitry Pyryeskin <dmitry@pyryeskin.com>
2017-07-16 23:47:07 -0400

    - Added function to get all picks by user, sorted by events - Updated view and template for matches

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Event, EventGroup, EventParticipant
+from .models import Event, EventGroup, EventParticipant, Pick
 
 admin.site.register(Event)
 admin.site.register(EventGroup)
 admin.site.register(EventParticipant)
+admin.site.register(Pick)
