42acff20c1aa45a81a4da1188f83b4e664fc7353
Dmitry Pyryeskin <dpyryeskin@eyereturn.com>
2017-07-25 15:31:23 -0400

    Added a postgres fix for Heroku (should be able to use postgis backend now)

--- bokaru/settings.py
+++ bokaru/settings.py
@@ -158,6 +158,8 @@ USE_TZ = True
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+# postgis fix for Heroku
+DATABASES['default']['NAME'] = DATABASES['default']['NAME'].replace('postgres', 'postgis', 1)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
