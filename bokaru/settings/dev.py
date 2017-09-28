60df7a3d76f70ec3a6de138d025791c5a9dfb7ea
Dmitry Pyryeskin <dmitry@pyryeskin.com>
2017-09-28 00:11:43 -0400

    - Updated database settings in prod

--- bokaru/settings/dev.py
+++ bokaru/settings/dev.py
@@ -17,7 +17,6 @@ MEDIA_URL = '/uploads/'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- bokaru/settings/prod.py
+++ bokaru/settings/prod.py
@@ -28,10 +28,14 @@ DEFAULT_FILE_STORAGE = 'bokaru.storage_backends.MediaStorage'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': dj_database_url.config(conn_max_age=500),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Update database configuration with $DATABASE_URL
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # ElastiCache
 CACHES = {
