f415a0c5bd3118c863cd4aab076b7238c2c78325
Dmitry Pyryeskin <dmitry@pyryeskin.com>
2018-01-19 00:07:03 -0500

    Updated URL patterns

--- account/urls.py
+++ account/urls.py
@@ -1,16 +1,17 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 app_name = 'account'
 urlpatterns = [
-    url(r'^profile$', views.view, name='view'),
-    url(r'^profile/view/(?P<username>\w+)$', views.view_user, name='view_user'),
-    url(r'^profile/edit$', views.edit, name='edit'),
-    url(r'^profile/edit/photo$', views.update_photo, name='update_photo'),
-    url(r'^account$', views.preferences, name='preferences'),
-    url(r'^account/payment$', views.preferences_payment, name='payment'),
-    url(r'^account/payment_history$', views.preferences_payment_history, name='payment_history'),
-    url(r'^account/close$', views.close, name='close'),
+    path('profile', views.view, name='view'),
+    path('profile/view/<str:username>', views.view_user, name='view_user'),
+    path('profile/edit', views.edit, name='edit'),
+    path('profile/edit/photo', views.update_photo, name='update_photo'),
+    path('account', views.preferences, name='preferences'),
+    path('account/payment', views.preferences_payment, name='payment'),
+    path('account/payment_history', views.preferences_payment_history, name='payment_history'),
+    path('account/close', views.close, name='close'),
     # Usage example: $.post( "/memo/about/2/", "lorem ipsum dolor sit amet");
-    url(r'^memo/about/(?P<about_user_id>\d+)/$', views.create_or_update_memo, name='memo')
+    path('memo/about/<str:about_user_id>/', views.create_or_update_memo, name='memo')
 ]
